f55b101641a4480761ea0f211fdbcdc65e3a9ecb
Evan Huang <evan.huang@berkeley.edu>
2023-03-09 14:00:08 -0800

    changed structure to just render in genotypeViewer and added more functionality for children

--- src/client/genotype/genotypeFamilyTreePage.tsx
+++ src/client/genotype/genotypeFamilyTreePage.tsx
@@ -1,52 +1,41 @@
-import TabsPage from '../bases/tabsPage';
-import type { Props, State, TabState } from '../bases/tabsPage';
 import type { Genotype, CellValue } from '../../server/database';
-import GenotypeSelector from './genotypeSelector';
 import GenotypeViewer from './genotypeViewer';
-import GenotypesPage from './genotypesPage';
+
 
 const upperLim: number = 50; // default value for the max number of generations to display
 
-class genotypeFamilyTreePage extends GenotypesPage {
-    constructor(props: Readonly<Props>) {
-        super(props);
-        this.props.jumpController.registerGenotypeJumpHandler(this.jumpToID.bind(this));
-    }
-    
-    override jumpToID(uid: string | number): void {
-        this.newTab();
-        this.selectGenotype(this.state.currentTab)(uid.toString());
-    }
+class genotypeFamilyTreePage {
+    private baseGenotype: Genotype;
+    private genotypesMap: Map<string, Genotype>;
+    private element: JSX.Element;
 
-    protected override renderTabContent(tabNum: number): JSX.Element {
-        // TODO: figure this out
-        if(this.state.tabs[tabNum] === undefined) {
-            return <div />
-        }
-
-        /*
-        if(this.state.tabs[tabNum].contentSelected) {
-            return <GenotypeViewer uid={ this.state.tabs[tabNum].contentID.toString() } jumpController={ this.props.jumpController } />
-        } else {
-            return <GenotypeSelector reportGenotype={ this.selectGenotype(tabNum) } />
-        }*/
+    private constructor(gv: GenotypeViewer, uid: string, width: number = upperLim, numParentGens: number = upperLim, numChildGens: number = upperLim) {
+        this.baseGenotype = gv.getGenotype() as Genotype;
+        this.genotypesMap = new Map<string, Genotype>(); // TODO: figure out getting map
+        this.element = <></>; // TODO: initialize element?
     }
 
-    
-    public displayLine(genotype: Genotype, width: number, numParentGens: number = upperLim, numChildGens: number = upperLim): void {
+    public static generateJSX(gv: GenotypeViewer, uid: number | string, width: number = upperLim, numParentGens: number = upperLim, numChildGens: number = upperLim): JSX.Element {
+        let gftp: genotypeFamilyTreePage = new genotypeFamilyTreePage(gv, uid.toString(), width, numParentGens, numChildGens);
+        // TODO: actually call functions
+        return gftp.element;
+    }
+
+    // TODO: change args to take in uid? and remove unnecessary args. 
+    private displayLine(width: number, numParentGens: number = upperLim, numChildGens: number = upperLim): void {
         
-        // TODO: the number parameters should all be optional
-        let genotypesMap = new Map<string, Genotype>; // TODO: access from database
-        this.displayParentLine(genotype, numParentGens, genotypesMap);
-        let siblings: Genotype[] = genotypeFamilyTreePage.getSiblings(genotype, genotypesMap, width);
+
+        this.displayParentLine(this.baseGenotype, numParentGens);
+        let siblings: Genotype[] = this.getSiblings(this.baseGenotype, width);
         // TODO: deal with siblings and children
-        let children: Genotype[] = genotypeFamilyTreePage.getChildren(genotype, genotypesMap, numChildGens);
+        let children: Genotype[] = this.getChildren(this.baseGenotype, numChildGens);
+
     }
 
     /**
-     * Helper function that returns the 2 parents of the given genotype in a Genotype array. Possibly could be moved to another file
+     * Helper function that returns the 2 parents of the given genotype in a Genotype array. 
      */
-    private static getParents(genotype: Genotype, genotypesMap: Map<string, Genotype>): (Genotype[]) {
+    private getParents(genotype: Genotype): (Genotype[]) {
         let parents : Genotype[] = new Array(2); 
         let motherID: CellValue = '';
         let fatherID: CellValue = '';
@@ -65,8 +54,8 @@ class genotypeFamilyTreePage extends GenotypesPage {
         // TODO: add error check here
         // will be either "AB", "RNF", or "gID-{x}", also check for None
         
-        parents[0] = genotypesMap.get(motherID as string) as Genotype;
-        parents[1] = genotypesMap.get(fatherID as string) as Genotype;
+        parents[0] = this.genotypesMap.get(motherID as string) as Genotype;
+        parents[1] = this.genotypesMap.get(fatherID as string) as Genotype;
         return parents;
     }
 
@@ -74,37 +63,55 @@ class genotypeFamilyTreePage extends GenotypesPage {
     /**
      * Recursive function that displays all parents, up to numGenerations generations
      */
-    private displayParentLine(genotype: Genotype, numGenerations: number, genotypesMap: Map<string, Genotype>): void  {
+    private displayParentLine(genotype: Genotype, numGenerations: number): void  {
         // base cases: AB, RNF (end of family line) or numGenerations == 0 or already displayed 
         // TODO: check already displayed condition and consider using generation number
         // TODO: numGenerations should be optional
         if(genotype.uid == 'AB' || genotype.uid == 'RNF' || numGenerations == 0 || this.displayedParents.includes(genotype)) { 
-            genotypeFamilyTreePage.displayGenotype(genotype);
+            this.displayGenotype(genotype);
             return;
         }
-        let parents: Genotype[] = genotypeFamilyTreePage.getParents(genotype, genotypesMap);
+        let parents: Genotype[] = this.getParents(genotype);
 
 
         // check for cycles using displayedParents list
         this.displayedParents.push(genotype);
         // need to check traversal order
-        genotypeFamilyTreePage.displayGenotype(parents[0]);
-        genotypeFamilyTreePage.displayGenotype(parents[1]);
+        this.displayGenotype(parents[0]);
+        this.displayGenotype(parents[1]);
 
-        this.displayParentLine(parents[0], numGenerations-1, genotypesMap);
-        this.displayParentLine(parents[1], numGenerations-1, genotypesMap);
+        this.displayParentLine(parents[0], numGenerations-1);
+        this.displayParentLine(parents[1], numGenerations-1);
         
     }
 
+    private displayedChildren: Genotype[] = []; // TODO: need to consider how to check if children (and parents) are the same and if to link
+    private displayChildrenLines(genotype: Genotype, numGenerations: number, maxPerGeneration: number): void {
+        const children: Genotype[] = this.getChildren(genotype, maxPerGeneration);
+        if(genotype.uid == 'AB' || genotype.uid == 'RNF' || numGenerations == 0 || this.displayedParents.includes(genotype) || children.length <= 0) { 
+            this.displayGenotype(genotype);
+            return;
+        }
+
+
+        for(let child of children) {
+            this.displayedChildren.push(child);
+            this.displayGenotype(child)
+        }
+        
+        for(let child of children) {
+            this.displayChildrenLines(child, numGenerations-1, maxPerGeneration);
+        }
+    }
 
     /**
      * Helper function to get a number of children for the given genotype. Possibly could be moved to another file
      */
-    private static getChildren(genotype: Genotype, genotypesMap: Map<string, Genotype>, maxToDisplay: number): Genotype[] {
+    private getChildren(genotype: Genotype, maxToDisplay: number): Genotype[] {
         let result: Genotype[] = [];
         let counter: number = 0;
-        for(let entry of Array.from(genotypesMap.entries())) {
-            let parents: Genotype[] = genotypeFamilyTreePage.getParents(entry[1], genotypesMap);
+        for(let entry of Array.from(this.genotypesMap.entries())) {
+            let parents: Genotype[] = this.getParents(entry[1]);
             if(parents[0].uid == genotype.uid || parents[1].uid == genotype.uid) {
                 result.push(entry[1]);
                 counter++;
@@ -120,17 +127,17 @@ class genotypeFamilyTreePage extends GenotypesPage {
      * Helper function to get a number of siblings for the given genotype. 
      * Assumes the "sibling" column has been added to genotypes. This will depend on the implementation of the sibling column, so it can be implemented later. 
      */
-    private static getSiblings(genotype: Genotype, genotypesMap: Map<string, Genotype>, width: number): Genotype[] {
+    private getSiblings(genotype: Genotype, width: number): Genotype[] {
         let result: Genotype[] = [];
         let counter: number = 0;
-        for(let entry of Array.from(genotypesMap.entries())) {
+        for(let entry of Array.from(this.genotypesMap.entries())) {
             for(let i = 0; i < entry[1].fields.length; i++) {
                 // TODO: this is wrong probably, will depend on implementation
-                if (entry[1].fields[i].label == 'siblings') {
+                if(entry[1].fields[i].label == 'siblings') {
                     result.push(entry[1]);
                     counter++;
                 }
-                if (counter >= width) break;
+                if(counter >= width) break;
             }
             
         }
@@ -141,11 +148,12 @@ class genotypeFamilyTreePage extends GenotypesPage {
     /**
      * Displays the genotype. For now, just log to console. 
      */
-    private static displayGenotype(genotype: Genotype): void {
+    private displayGenotype(genotype: Genotype): void {
         console.log(genotype.uid + " ");
+        // TODO: add genotype to the element
     }   
 
 }
 
-export default genotypeFamilyTreePage
+export default genotypeFamilyTreePage.generateJSX;
 
