87357d11d4e7859e8766c475ea8b870477890c3c
Evan Huang <evan.huang@berkeley.edu>
2023-04-17 16:45:21 -0700

    working on rendering table

--- src/client/genotype/crossingPage.tsx
+++ src/client/genotype/crossingPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Genotype } from "../../server/database";
+import { Td, Text, Textarea, Tr } from '@chakra-ui/react';
+import { CellValue, Field, Genotype } from "../../server/database";
 import JumpController from "../jumpController";
 
 
@@ -19,7 +20,9 @@ class CrossingPage extends React.Component<Props, State> {
     constructor(props: Readonly<Props>) {
         super(props);
         this.state = {
-            child: undefined
+            child: undefined,
+            mother: undefined,
+            father: undefined,
         };
     }
 
@@ -49,11 +52,71 @@ class CrossingPage extends React.Component<Props, State> {
 
     }
 
+    private saveUID(uid: string): void {
+        // TODO: make sure UID is not taken already. autofill?
+    }
+
+    private saveTanks(): void {
+        // TODO: check how to implement this
+    }
+
     /**
      * Saves child Genotype back to database. 
      */
     private saveGenotype(): void {
+        
+    }
+    /**
+     * Converts the Genotype object's fields *other* than `fields` to JSX
+     */
+    private metadataToJSX(): JSX.Element[] {
+        if(this.state.child === undefined || this.state.mother === undefined || this.state.father === undefined) {
+            return [ <div>loading</div>, ];
+        }
 
+        return [
+            <Tr key='uid'>
+                <Td>UID</Td>
+                <Td>
+                    { this.state.mother.uid }
+                </Td>
+                <Td>
+                    { this.state.father.uid }
+                </Td>
+                <Td>
+                { 
+                    <Textarea value={ this.state.child.uid } rows={ 1 } 
+                            onChange={ (e): void => { this.saveUID(e.target.value) } }
+                    />
+                }
+                </Td>
+            </Tr>,
+            <Tr key='tanks'>
+                <Td>tanks</Td>
+                <Td>
+                    { this.props.jumpController.embedJumps(this.state.mother.tanks.map(
+                            (uid: number): string => `\\T${uid}` ).join('\n'))
+                    }
+                </Td>
+                <Td>
+                    { this.props.jumpController.embedJumps(this.state.father.tanks.map(
+                            (uid: number): string => `\\T${uid}` ).join('\n'))
+                    }
+                </Td>
+            </Tr>,
+        ];
+    }
+
+    private generateJSX(fields: Field[]): JSX.Element {
+        return <></>;
+    }
+
+    override render(): JSX.Element {
+        if(this.state?.child !== undefined) {
+            return this.generateJSX(this.state.child.fields);
+        } else {
+            return <Text>loading</Text>;
+        }
     }
 }
 
