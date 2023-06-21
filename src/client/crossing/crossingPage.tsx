e26a47e63f20de0dc29ee93f05d0d66b3e802ebc
Evan Huang <evan.huang@berkeley.edu>
2023-06-21 14:55:13 -0700

    parent selection is functional

--- src/client/crossing/crossingPage.tsx
+++ src/client/crossing/crossingPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import JumpController from "../jumpController";
 import CrossingTable from "./crossingTable";
 import GenotypeSelector from "../genotype/genotypeSelector";
+import { Stack, Text } from "@chakra-ui/react";
 
 type Props = {
     jumpController: JumpController,
@@ -13,20 +14,29 @@ type State = {
 };
 
 class CrossingPage extends React.Component<Props, State> {
-
+    constructor(props: Readonly<Props>) {
+        super(props);   
+        this.state = {
+            motherId: undefined,
+            fatherId: undefined,
+        }
+    }
+    /**
+     * Sets the selected parent ID to the selected uid.
+     */
     private selectParent(parent: number): (uid: string) => void {
         return (uid: string): void => {
             if(parent === 0) {
                 this.setState((state: Readonly<State>): Readonly<State> => {                    
                     return {
                         motherId: uid,
-                        fatherId: this.state.fatherId,
+                        fatherId: state.fatherId,
                     }
                 });
             } else {
                 this.setState((state: Readonly<State>): Readonly<State> => {                    
                     return {
-                        motherId: this.state.motherId,
+                        motherId: state.motherId,
                         fatherId: uid,
                     }
                 });
@@ -36,14 +46,20 @@ class CrossingPage extends React.Component<Props, State> {
 
     override render(): JSX.Element {
         if(this.state.motherId === undefined) {
-            return <GenotypeSelector reportGenotype={ this.selectParent(0) } />
+            return (<Stack>
+                <Text fontSize={'x-large'} textColor={'blue'}>Please select a mother. </Text>
+                <GenotypeSelector reportGenotype={ this.selectParent(0) } />
+            </Stack>);
         }
         
         if(this.state.fatherId === undefined) {
-            return <GenotypeSelector reportGenotype={ this.selectParent(1) } />
+            return (<Stack>
+                <Text fontSize={'x-large'} textColor={'blue'}>Please select a father. </Text>
+                <GenotypeSelector reportGenotype={ this.selectParent(1) } />
+            </Stack>);
         }
 
-        return <CrossingTable motherId = { this.state.motherId } fatherId = { this.state.fatherId } jumpController={ this.props.jumpController } />
+        return (<CrossingTable motherId = { this.state.motherId } fatherId = { this.state.fatherId } jumpController={ this.props.jumpController } />);
     }
 
 }
