6a9d4b7040180ebdc523d681eda7a29ff9bda0c7
Evan Huang <evan.huang@berkeley.edu>
2023-08-02 13:44:07 -0700

    export graveyard

--- src/server/export_sqlite_to_csv.sh
+++ src/server/export_sqlite_to_csv.sh
@@ -5,8 +5,11 @@ DB_FILE="./data/sqlite3.db"  # Replace with the path to your SQLite database fil
 # SQLite query to export the table to CSV
 QUERY_TANKS="SELECT * FROM tanks;"
 QUERY_GENOTYPES="SELECT * FROM genotypes;"
+QUERY_GRAVEYARD="SELECT * FROM graveyard;"
 TANKS_OUTPUT="./data/exported_tanks.csv"
 GENOTYPES_OUTPUT="./data/exported_genotypes.csv"
+GRAVEYARD_OUTPUT="./data/exported_graveyard.csv"
+
 
 # Run SQLite and execute the tank query, then redirect the output to the CSV files
 sqlite3 -header -csv ${DB_FILE} "${QUERY_TANKS}" > ${TANKS_OUTPUT}
@@ -26,3 +29,10 @@ else
   echo "Error exporting table 'genotypes' to CSV."
 fi
 
+sqlite3 -header -csv ${DB_FILE} "${QUERY_GRAVEYARD}" > ${GRAVEYARD_OUTPUT}
+# Check if the export was successful
+if [ $? -eq 0 ]; then
+  echo "Table 'graveyard' exported to '${GRAVEYARD_OUTPUT}' successfully."
+else
+  echo "Error exporting table 'graveyard' to CSV."
+fi
